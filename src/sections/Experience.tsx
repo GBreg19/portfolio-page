4204d6cf1b7097d3dff4995d1bd1be3b47e0c3d1
Giorgi Bregvadze <giorgi.bregvadze@nari.ge>
2024-03-01 10:53:12 +0400

    changing commit messages

--- src/sections/Experience.tsx
+++ src/sections/Experience.tsx
@@ -13,13 +13,16 @@ const Experience = () => {
         <h1 className="font-spaceMonoBoldIta text-3xl text-white">
           Working Experience
         </h1>
-        <div className="flex flex-col ">
+        <div className="flex flex-col">
           <h1 className="font-spaceMonoBold text-xl text-white mb-1">
             Nari Solutions
           </h1>
-          <h2 className="font-spaceMonoIta">
-            Frontend Developer Intern - Nov 2023 - Present
-          </h2>
+          <span className="flex  items-center gap-5">
+            <h2 className="font-spaceMonoIta">Junior Frontend Developer</h2>
+            <p className="font-vtReg bg-slate-700 px-2 rounded-md text-sm tracking-wide text-white">
+              Nov 2023 - Present
+            </p>
+          </span>
         </div>
       </section>
     </Card>
